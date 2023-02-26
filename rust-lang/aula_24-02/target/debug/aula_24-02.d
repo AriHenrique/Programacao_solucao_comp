86c75973187ad4d98c69237785ca1ece12b77ad8
AriHenrique <arihenriqueuna@gmail.com>
2023-02-26 20:59:20 -0300

    reformulando pastas rust

--- rust-lang/aula_24-02/target/debug/aula_24-02.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/arihe/developer/programacao_solucoes_comp/rust-lang/aula_24-02/target/debug/aula_24-02: /mnt/c/Users/arihe/developer/programacao_solucoes_comp/rust-lang/aula_24-02/src/main.rs
